86e018fd00c633f75ae7e4abd60b6ab99849bb97
isalmirsg1985 <isalmir@gmail.com>
2023-10-22 01:58:50 -0300

    Envio RateioCondomino.java

---
+++ RateioCondominio.java
@@ -0,0 +1,32 @@
+package aula01programacaoavancada;
+
+import javax.swing.JOptionPane;
+
+public class Aula01ProgramacaoAvancada {
+
+    public static void main(String[] args) {
+        
+        String totalfaturaString = JOptionPane.showInputDialog("Digite o valor total da fatura: ");
+        int totalfatura = Integer.parseInt(totalfaturaString);
+
+        String consumoaguaString = JOptionPane.showInputDialog("Digite a quantidade de agua em m3 consumida: ");
+        int consumoagua = Integer.parseInt(consumoaguaString);
+        
+        String quantidadeString = JOptionPane.showInputDialog("Digite a quantidade de apartamentos: ");
+        int quantidade = Integer.parseInt(quantidadeString);
+                        
+            for(int i = 1; i <= quantidade; i++){
+                String apartString = JOptionPane.showInputDialog(" Digite a quantidade consumida do apartamento " + i);
+                int apart = Integer.parseInt(apartString);
+                int total = (totalfatura * apart) / consumoagua;
+                
+                JOptionPane.showMessageDialog(null,"O apartamento " + i + " pagara " + total);
+                
+        
+        }
+        
+    }
+        
+}
+    
+
